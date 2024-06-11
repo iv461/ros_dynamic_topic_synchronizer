b144b2875aab20f6706ad82af8bb1e424c5afa3f
ivo <ivo.ivanov@hs-augsburg.de>
2024-06-11 19:00:05 +0200

    Added copyright notice

--- example/example_listener_node.cpp
+++ example/example_listener_node.cpp
@@ -1,3 +1,20 @@
+
+/*
+Copyright © 2024 StarkStrom Augsburg
+Author: Ivo Ivanov
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #include <ros/ros.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/Image.h>
@@ -51,7 +68,5 @@ int main(int argc, char **argv) {
       {{{"/image1", "/image2"}, {"/camera_info1", "/camera_info2"}, {"/point_cloud"}}}, 1);
 
   ros::spin();
-  //ROS_INFO("Subscribed to: "<< "")
-
   return 0;
 }
